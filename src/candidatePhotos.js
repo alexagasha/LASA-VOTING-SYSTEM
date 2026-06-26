09d41bd1b7da580cab2fca979a2f3740064ecdd9
AGASHA ALEX <agashaalex@gmail.com>
2026-06-26 23:25:30 +0300

    Add candidate photos via direct image imports

---
+++ src/candidatePhotos.js
@@ -0,0 +1,15 @@
+// candidatePhotos.js
+// Import candidate photos directly from the src folder.
+// Mapped by candidate ID: 1 = AMONGI DOLLY DIANA, 2 = ARAC BENEDICT, 3 = ACENG DELIGHT RUTH
+
+import AMONGI from "./AMONGI.jpeg";
+import ARAC from "./ARAC.jpeg";
+import DELIGHT from "./DELIGHT.jpeg";
+
+const CANDIDATE_PHOTOS = {
+  1: AMONGI,
+  2: ARAC,
+  3: DELIGHT,
+};
+
+export default CANDIDATE_PHOTOS;
